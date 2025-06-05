75459d1c298c6f7bad15aba7d320e129428e8a5c
TBejarano24 <bejaranotomas04@gmail.com>
2025-06-05 01:50:44 -0300

    Se acabaron los detalles de responsiveness y se agregó mayor dinamismo a la página. Sólo resta configurar el resto de los datos dinámicos, permitir que la etiqueta Link se despliegue en una nueva pestaña y agregar funcionalidad a la barra de búsqueda

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,58 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 export default function Layout() {
+  const [profileData, setProfileData] = useState("");
+  const [profileRepos, setProfileRepos] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get("https://api.github.com/users/github");
+        setProfileData(response.data);
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      if (!profileData.repos_url) return;
+
+      try {
+        setLoading(true);
+        const response = await axios.get(`${profileData.repos_url}`);
+        setProfileRepos(response.data.slice(0, 4));
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRepos();
+  }, [profileData.repos_url]);
+
+  if (loading) {
+    console.log("loading...");
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
+  console.log(profileData);
+  console.log(profileRepos);
+
   return (
     <div className="bg-[#20293A]">
       <section className="h-[230px] w-full bg-[url(/hero-image-github-profile.png)] bg-cover bg-center flex justify-center">
@@ -21,67 +73,84 @@ export default function Layout() {
         </div>
       </section>
       <section className="h-auto flex flex-col justify-center w-[85%] mx-auto">
-        <div className="absolute h-[120px] w-[120px] bg-[#20293A] top-[190px] left-[30px] rounded-xl flex justify-center items-center">
+        <div className="absolute h-[120px] w-[120px] bg-[#20293A] top-[190px] left-[7%] rounded-xl flex justify-center items-center">
           <img
-            className="rounded-xl"
-            src="https://placecats.com/millie_neo/105/105"
+            className="rounded-xl w-[105px] h-[105px]"
+            src={profileData.avatar_url}
             alt="profile picture"
           />
         </div>
-        <div className="flex flex-wrap gap-5 w-[70%] mt-[120px]">
+        <div className="flex flex-wrap gap-5 w-[70%] mt-[120px] lg:mt-[12px] lg:ml-[140px]">
           <div className="bg-[#111729] h-[50px] w-auto flex justify-center items-center rounded-xl text-[#CDD5E0] px-5">
             <span className="border-r border-[#CDD5E0] pr-3">Followers</span>
-            <span className="pl-3">27389</span>
+            <span className="pl-3">{profileData.followers}</span>
           </div>
           <div className="bg-[#111729] h-[50px] w-auto flex justify-center items-center rounded-xl text-[#CDD5E0] px-5">
             <span className="border-r border-[#CDD5E0] pr-3">Following</span>
-            <span className="pl-3">0</span>
+            <span className="pl-3">{profileData.following}</span>
           </div>
-          <div className="bg-[#111729] h-[50px] w-auto min-w-[250px] flex justify-center items-center rounded-xl text-[#CDD5E0] px-5">
-            <span className="border-r border-[#CDD5E0] pr-3">Location</span>
-            <span className="pl-3">San Francisco, CA</span>
-          </div>
+
+          {profileData?.location ? (
+            <div className="bg-[#111729] h-[50px] w-auto min-w-[250px] flex justify-center items-center rounded-xl text-[#CDD5E0] px-5">
+              <span className="border-r border-[#CDD5E0] pr-3">Location</span>
+              <span className="pl-3">{profileData.location}</span>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         <section id="user-name-bio" className="py-5">
-          <h1 className="text-[#CDD5E0] text-[30px] font-semibold">GitHub</h1>
+          <h1 className="text-[#CDD5E0] text-[30px] font-semibold">
+            {profileData?.name ? profileData.name : profileData?.login}
+          </h1>
           <p className="text-[#CDD5E0] font-semibold">
-            How people build software
+            {profileData?.bio ? profileData.bio : ""}
           </p>
         </section>
         <section
           id="card-container"
-          className="w-full grid grid-cols-1 gap-y-6"
+          className="w-full grid grid-cols-1 gap-y-6 lg:grid-cols-2 lg:gap-x-6"
         >
-          <div className="h-auto w-full p-[20px_4px] bg-[#1D1B48] rounded-xl flex flex-col justify-center items-center">
-            <div className="w-[90%] flex flex-col gap-4">
-              <h2 className="text-[#CDD5E0] text-[18px] font-semibold">
-                .github
-              </h2>
-              <p className="text-[#97A3B6] text-[17px]">
-                Community health files for the @GitHub organization
-              </p>
-              <div className="flex w-full gap-3">
-                <div className="flex font-semibold items-center gap-1">
-                  <img src="/Nesting.svg" alt="nesting" />
-                  <span className="text-[#97A3B6] text-[17px]">2,369</span>
-                </div>
-                <div className="flex font-semibold items-center gap-1">
-                  <img src="/Star.svg" alt="star" />
-                  <span className="text-[#97A3B6] text-[17px]">703</span>
+          {profileRepos?.map((repo) => {
+            return (
+              <div className="h-auto w-full p-[20px_4px] bg-[#1D1B48] rounded-xl flex flex-col justify-center items-center">
+                <div className="w-[90%] flex flex-col gap-4">
+                  <h2 className="text-[#CDD5E0] text-[18px] font-semibold">
+                    {repo?.name}
+                  </h2>
+                  <p className="text-[#97A3B6] text-[17px]">
+                    {repo?.description}
+                  </p>
+                  <div className="flex w-full gap-3">
+                    {repo?.forks ? (
+                      <div className="flex font-semibold items-center gap-1">
+                        <img src="/Nesting.svg" alt="nesting" />
+                        <span className="text-[#97A3B6] text-[17px]">
+                          {repo?.forks_count}
+                        </span>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    <div className="flex font-semibold items-center gap-1">
+                      <img src="/Star.svg" alt="star" />
+                      <span className="text-[#97A3B6] text-[17px]">703</span>
+                    </div>
+                  </div>
+                  <span className="text-[#97A3B6] text-[12px]">
+                    Updated 4 days ago
+                  </span>
                 </div>
               </div>
-              <span className="text-[#97A3B6] text-[12px]">
-                Updated 4 days ago
-              </span>
-            </div>
-          </div>
+            );
+          })}
         </section>
-        <a
+        <Link
           className="text-[#CDD5E0] text-[17px] text-center font-semibold mx-auto my-[60px]"
-          href="/"
+          to={"https://github.com"}
         >
           View all repositories
-        </a>
+        </Link>
       </section>
     </div>
   );
